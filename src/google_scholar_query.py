6c74f367b4415e5a99564690774d700ea94bd343
ian Shen <ianshan0915@gmail.com>
2018-08-28 09:51:32 +0200

    update google_scholar_query

--- src/google_scholar_query.py
+++ src/google_scholar_query.py
@@ -14,6 +14,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from scholar import ScholarConf, SearchScholarQuery, ScholarQuerier, ScholarSettings
 
+import time
+
 import pandas as pd
 
 def load_repos():
@@ -21,42 +23,84 @@ def load_repos():
     load the repository and do some basic filtering
     """
 
-    repos = pd.read_csv('')
+    repos = pd.read_csv('/Users/ianshen/Documents/github/clinical-opensource-projects/data/repos_topics.csv')
+    repos = repos.loc[repos['forks']>0]
+    # repos = repos.loc[repos['full_name']=='smistad/FAST']
 
     return repos
 
-def test():
+def get_query_terms(repos):
     """
-    perform a test query with a given phrase
+    get list of query terms for each repo, including phrase and start_time
     """
 
-    querier = ScholarQuerier()
-    settings = ScholarSettings()
-    settings.set_citation_format(ScholarSettings.CITFORM_BIBTEX)
-    querier.apply_settings(settings)
+    repos['created_at'] = pd.to_datetime(repos['created_at'])
+    repos['created_year'] = repos.created_at.dt.year
+    query_terms = repos[['id','full_name', 'created_year']]
 
-    query = SearchScholarQuery()
-    query.set_phrase('commontk/CTK') # commontk/CTK, meoyo/AIPS
-    query.set_timeframe('2008')
-    querier.send_query(query)
-    articles = querier.articles
+    return query_terms
 
-    return articles
-
-def main():
+def process_arts(config, repo_id, phrase, articles):
     """
-    do something
+    process return articles object, return a list of articles with their basic information
     """
-    config = ScholarConf()
-    articles = test()
+    arts = []
     for art in articles:
         # res = art.as_csv()
         keys = [pair[0] for pair in sorted([(key, val[2]) for key, val in \
                 list(art.attrs.items())], key=lambda pair: pair[1])]
         res = [art.attrs[key][0] for key in keys]
-        if config.SCHOLAR_SITE in res[1]: # the second element is the url of the paper
-            res[1] = res[1].replace(config.SCHOLAR_SITE, '')
-        print(res)
+        if res[1]: # first check if url is NoneType or not
+            if config.SCHOLAR_SITE in res[1]: # the second element is the url of the paper
+                res[1] = res[1].replace(config.SCHOLAR_SITE + '/', '')
+        res = [repo_id] + [phrase] + res
+        arts.append(res)
+
+    return arts
+
+def literature_search(query_terms):
+    """
+    perform a google scholar query with given terms
+    """
+
+    querier = ScholarQuerier()
+    settings = ScholarSettings()
+    config = ScholarConf()
+    settings.set_citation_format(ScholarSettings.CITFORM_BIBTEX)
+    querier.apply_settings(settings)
+    query = SearchScholarQuery()
+
+    papers = []
+    for item in query_terms.values:
+        repo_id = item[0]
+        phrase = item[1]
+        start_year = item[2]
+        query.set_phrase(phrase) # commontk/CTK, meoyo/AIPS
+        query.set_timeframe(start_year)
+        print('search papers for {} ...'.format(phrase))
+        querier.send_query(query)
+        articles = querier.articles
+        if len(articles)==0:
+            continue
+        results = process_arts(config, item[0], phrase, articles)
+        papers = papers + results
+        time.sleep(1)
+
+    return papers
+
+def main():
+    """
+    do something
+    """
+    repos = load_repos()
+    query_terms = get_query_terms(repos)
+    query_terms = query_terms.head(100)
+    papers = literature_search(query_terms)
+    col_names = ['id','full_name','title','url','year','citations','versions','cluster_id', 'url_pdf','url_citations', \
+                 'url_version', 'url_citation','excerpt']
+    papers_df = pd.DataFrame(papers, columns=col_names)
+    # papers_df.head()
+    papers_df.to_csv('~/Documents/papers_test.csv', index=False)
 
 if __name__ == "__main__":
     main()
